b60d499eeae1e0ac0a7d52c1d2296aa4f8d9ed5b
goldenbox6898-alt <golden.box6898@gmail.com>
2026-07-23 13:17:16 +0330

    Update main.py

--- main.py
+++ main.py
@@ -485,14 +485,6 @@ app.add_handler(
     group=0
 )
 
-# مدیریت سیگنال
-app.add_handler(
-    MessageHandler(
-        filters.TEXT & ~filters.COMMAND,
-        receive_signal
-    ),
-    group=1
-)
 
 # دریافت رسید پرداخت
 app.add_handler(
